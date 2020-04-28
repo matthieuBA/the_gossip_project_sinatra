127878af1d2db536cf3dba6c8f6770a4f27ce780
matthieuBA <matthieubacheley@gmail.com>
2020-04-28 08:45:55 +0200

    final

--- lib/controller.rb
+++ lib/controller.rb
@@ -17,19 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/gossip/:id" do
-    # matches "GET /hello/foo" and "GET /hello/bar"
-    # params['name'] is 'foo' or 'bar'
     puts "Voici le numéro du potin que tu veux : #{params["id"]}!"
-    # Gossip.find(params["id"])
     erb :show, locals: { gossip: Gossip.find(params["id"]) }
   end
 
   post "/gossip/:id" do
     puts "Voici le numéro du potin à commenter : #{params["id"]}!"
-    #Gossip.update(params["id"])
     Gossip.comment(params["id"], params["gossip_comment"])
-    # erb :edit, locals: { gossip: Gossip.find(params["id"]) }
-    # Gossip.update(params["gossip_author"], params["gossip_content"]).save
     redirect "/gossip/#{params["id"]}"
   end
 
@@ -39,11 +33,7 @@ class ApplicationController < Sinatra::Base
 
   post "/gossips/:id/edit/" do
     puts "Voici le numéro du potin que tu veux mettre à jour : #{params["id"]}!"
-    #Gossip.update(params["id"])
     Gossip.update(params["id"], params["gossip_author"], params["gossip_content"])
-    # erb :edit, locals: { gossip: Gossip.find(params["id"]) }
-    # Gossip.update(params["gossip_author"], params["gossip_content"]).save
     redirect "/"
   end
-  # run! if app_file == $0
 end
